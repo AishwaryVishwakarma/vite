290cd2057b0c556f902a3f71ab57c910056a3e85
AishwaryVishwakarma <aishwaryvishwakarma23@gmail.com>
2024-11-12 17:39:13 +0530

    using extends for eslint instead of spread

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,11 @@ import baseConfig from './configs/eslint.base.config.js';
 export default tseslint.config(
   {ignores: ['dist']},
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      baseConfig,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -18,12 +22,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      // Extend plugins from base config
-      ...baseConfig.plugins,
     },
     rules: {
-      // Extend rules from base config
-      ...baseConfig.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
